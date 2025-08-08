453230c69a6cdc54ee889ca9a42b6cebbb27dd66
nvonil <itslinonavarro@gmail.com>
2025-08-07 21:25:12 -0700

    Added logic for popup visibility and close

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello");
+const addButton = document.querySelector(".add-button");
+const cancelButton = document.querySelector(".cancel-button");
+
+const popupWrapper = document.querySelector(".popup-wrapper");
+const popupContainer = document.querySelector(".popup-container");
+
+addButton.addEventListener("click", openPopup);
+cancelButton.addEventListener("click", closePopup);
+popupWrapper.addEventListener("click", function (e) {
+    if (e.target === popupWrapper) {
+        closePopup();
+    }
+});
+
+function openPopup() {
+    popupWrapper.classList.add("visible");
+    popupContainer.classList.add("visible");
+}
+
+function closePopup() {
+    popupWrapper.classList.remove("visible");
+    popupContainer.classList.remove("visible");
+}
